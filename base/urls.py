c3a263139ba96fd172333c592416a41713b811d7
Athika Priya <athikapriya1997@gmail.com>
2026-03-17 01:09:33 +0600

    Add URL routes for CURD and homepage

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,14 @@
+# third party imports
 from django.urls import path
 
+# local imports
 from . import views
+
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("toggle/<int:pk>/", views.toggle_complete, name="toggle_complete"),
+    path("create_task/", views.create_task, name="create_task"),
+    path("update_task/<int:pk>/", views.update_task, name="update_task"),
+    path("delete_task/<int:pk>/", views.delete_task, name="delete_task"),
 ]
